788cf5bd2658c52725dbfb8b0f85ad15d29eaf9f
Andrea <andrea.klaus.tubak@gmail.com>
2021-05-12 10:39:26 +0200

    Reserved username check

--- TrainingStat/app/src/main/java/it/unipi/dii/trainingstat/gui/MainActivity.java
+++ TrainingStat/app/src/main/java/it/unipi/dii/trainingstat/gui/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,16 +26,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    // triggered only if confirmation button is clicked
     @Override
     public void onClick(View v) {
 
         EditText UsernameInput = findViewById(R.id.editTextUsernameName);
-
         String Username = UsernameInput.getText().toString();
 
-        Intent i = new Intent(this, MenuActivity.class);
-        i.putExtra("username", Username);
-        startActivity(i);
+        // the username "aggregatedResults" is not usable
+        if (Username.equals("aggregatedResults")){
+
+            Toast.makeText(this, "Username not valid", Toast.LENGTH_SHORT).show();
+
+        }else {
+
+            Intent i = new Intent(this, MenuActivity.class);
+            i.putExtra("username", Username);
+            startActivity(i);
+        }
     }
 }
