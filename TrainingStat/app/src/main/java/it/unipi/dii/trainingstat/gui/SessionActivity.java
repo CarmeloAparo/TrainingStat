aafd9f87476b9024ef3b9615f17f34ffc011e2b0
Andrea <andrea.klaus.tubak@gmail.com>
2021-05-13 14:52:17 +0200

    [WIP] Step sensor not found

--- TrainingStat/app/src/main/java/it/unipi/dii/trainingstat/gui/SessionActivity.java
+++ TrainingStat/app/src/main/java/it/unipi/dii/trainingstat/gui/SessionActivity.java
@@ -1,23 +1,47 @@
 package it.unipi.dii.trainingstat.gui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+<<<<<<< Updated upstream:TrainingStat/app/src/main/java/it/unipi/dii/trainingstat/gui/SessionActivity.java
 import it.unipi.dii.trainingstat.R;
 
 public class SessionActivity extends AppCompatActivity {
+=======
+import java.util.ArrayList;
+import java.util.List;
+>>>>>>> Stashed changes:TrainingStat/app/src/main/java/it/unipi/dii/trainingstat/SessionActivity.java
 
+public class SessionActivity extends AppCompatActivity implements SensorEventListener {
+
+    private int ACTIVITY_PERMISSION_CODE = 0;
+    private final String TAG = "SessionActivity";
     private Chronometer chronometer;
     private long pauseOffset; // serve per tenere traccia del tempo contato prima di cliccare pausa
-    private boolean running;
+    private boolean chronoRunning;
+
     private TextView StatusTV;
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
+    private int stepCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,15 +64,31 @@ public class SessionActivity extends AppCompatActivity {
 
         chronometer = findViewById(R.id.sessionChronometer);
 
+        requestPermissions();
+
+        sensorSetup();
     }
 
-    // calls the right handler method depending on the state of the button
-    public void startPauseButtonClicked(View view){
-        Button b = (Button)view;
 
-        if(!running) startButtonClicked(b);
+    private void sensorSetup() {
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        if (stepSensor == null) {
+            Toast.makeText(this, "Step sensor is not present", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+    }
+
+
+    // calls the right handler method depending on the state of the button
+    public void startPauseButtonClicked(View view) {
+        Button b = (Button) view;
+
+        if (!chronoRunning) startButtonClicked(b);
         else pauseButtonClicked(b);
-        running = !running;
+        chronoRunning = !chronoRunning;
     }
 
 
@@ -66,7 +106,7 @@ public class SessionActivity extends AppCompatActivity {
     }
 
 
-    private void pauseButtonClicked(Button startPauseButton){
+    private void pauseButtonClicked(Button startPauseButton) {
 
         StatusTV.setText("Paused");
         startPauseButton.setText(R.string.start_button_text);
@@ -87,12 +127,86 @@ public class SessionActivity extends AppCompatActivity {
         chronometer.setBase(SystemClock.elapsedRealtime());
         pauseOffset = 0;
 
+        // reset del counter
+        stepCount = 0;
+        TextView stepCounterTV = (TextView) findViewById(R.id.sessionStepCounterTV);
+        stepCounterTV.setText(String.valueOf(stepCount));
+
         /*
         TODO bloccare il timer,
           chiudere la sessione,
           chiudere l'attività corrente,
            passare all'attività di visualizzazione delle statistiche
         */
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == stepSensor && chronoRunning) {
+            stepCount = (int) event.values[0];
+            TextView stepCounterTV = (TextView) findViewById(R.id.sessionStepCounterTV);
+            stepCounterTV.setText(String.valueOf(stepCount));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.i(TAG, "Accuracy changed");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_GAME);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this, stepSensor);
+    }
+
+
+    // GESTIONE PERMESSI
+
+    private void requestPermissions() {
+        List<String> permissionsToRequest = new ArrayList<>();
+
+        if (!hasActivityRecognitionPermission()) {
+            permissionsToRequest.add(Manifest.permission.ACTIVITY_RECOGNITION);
+        }
+
+        if (!permissionsToRequest.isEmpty()) {
+            ActivityCompat.requestPermissions(this,
+                    permissionsToRequest.toArray(new String[0]),
+                    ACTIVITY_PERMISSION_CODE);
+        }
+    }
+
+
+    private boolean hasActivityRecognitionPermission() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode,
+            @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == ACTIVITY_PERMISSION_CODE && grantResults.length != 0){
+            for (int i = 0; i< grantResults.length; i++){
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    Log.i("Permission request", permissions[i] + " granted");
+                }
+            }
+        }
 
     }
 
