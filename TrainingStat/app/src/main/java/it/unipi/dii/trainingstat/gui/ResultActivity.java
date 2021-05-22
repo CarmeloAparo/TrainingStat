9e5923d263a4b53b7ba0952bf88f4997d30f3dcd
francesco.ronchieri <francesco.ronchieri@imagicle.com>
2021-05-22 14:54:00 +0200

    ResultActivity activity time - change TextView to Chronometer

--- TrainingStat/app/src/main/java/it/unipi/dii/trainingstat/gui/ResultActivity.java
+++ TrainingStat/app/src/main/java/it/unipi/dii/trainingstat/gui/ResultActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,7 +24,7 @@ public class ResultActivity extends AppCompatActivity {
     private TextView _trainingSessionIdTV;
     private TextView _startTimeTV;
     private TextView _endTimeTV;
-    private TextView _totalActivityTV;
+    private Chronometer _totalActivityChrono;
     private TextView _totalStepsTV;
     private TextView _stillPercentageTV;
     private TextView _walkingPercentageTV;
@@ -51,7 +53,7 @@ public class ResultActivity extends AppCompatActivity {
         _trainingSessionIdTV = findViewById(R.id.tv_session_id);
         _startTimeTV = findViewById(R.id.tv_start_time);
         _endTimeTV = findViewById(R.id.tv_end_time);
-        _totalActivityTV = findViewById(R.id.tv_total_activity_time);
+        _totalActivityChrono = findViewById(R.id.chrono_totalactivity_time);
         _totalStepsTV = findViewById(R.id.tv_total_steps);
         _stillPercentageTV = findViewById(R.id.tv_percentage_still);
         _walkingPercentageTV = findViewById(R.id.tv_percentage_walking);
@@ -66,12 +68,7 @@ public class ResultActivity extends AppCompatActivity {
         _trainingSessionIdTV.setText(_trainingSessionId);
         _startTimeTV.setText(TSDateUtils.DateInLocalTimezoneHumanReadable(_userSession.getStartDate()));
         _endTimeTV.setText(TSDateUtils.DateInLocalTimezoneHumanReadable(_userSession.getEndDate()));
-        Duration totalTime = Duration.ofMillis(_userSession.getTotalActivityTime());
-        String formattedTotalTime = totalTime.toString()
-                .substring(2)
-                .replaceAll("(\\d[HMS])(?!$)", "$1 ")
-                .toLowerCase();
-        _totalActivityTV.setText(formattedTotalTime);
+        _totalActivityChrono.setBase(SystemClock.elapsedRealtime() - _userSession.getTotalActivityTime());
         _totalStepsTV.setText(String.valueOf(_userSession.getTotSteps()));
         _stillPercentageTV.setText(String.format("%.2f",_userSession.getStillPerc()) + " %");
         _walkingPercentageTV.setText(String.format("%.2f",_userSession.getWalkPerc()) + " %");
